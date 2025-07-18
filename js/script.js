1709c409e7b608cbf627509111d1f3adf769ddae
marioamriansyah <marioamriansyah7@gmail.com>
2025-07-18 19:08:13 +0700

    add our profile and visimisi

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('messageForm');
-  const resultDiv = document.getElementById('result');
-  const currentTimeEl = document.getElementById('currentTime');
+    const form = document.getElementById('messageForm');
+    const resultDiv = document.getElementById('result');
+    const currentTimeEl = document.getElementById('currentTime');
+    const nama = "Mario Amriansyah";
 
-  function updateTime() {
-    const now = new Date();
-    currentTimeEl.textContent = now.toString();
-  }
+    const el = document.getElementById("welcomeText");
+    el.innerText = `Hi ${nama}, Welcome to Website`;
 
-  updateTime();
-
-  form.addEventListener('submit', function (e) {
-    e.preventDefault(); // ⛔ Hindari reload
-
-    const name = document.getElementById('name').value;
-    const dob = document.getElementById('dob').value;
-    const pesan = document.getElementById('pesan').value;
-    const gender = form.querySelector('input[name="gender"]:checked').value;
+    function updateTime() {
+        const now = new Date();
+        currentTimeEl.textContent = now.toString();
+    }
 
     updateTime();
 
-    resultDiv.innerHTML = `
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const dob = document.getElementById('dob').value;
+        const pesan = document.getElementById('pesan').value;
+        const gender = form.querySelector('input[name="gender"]:checked').value;
+
+        updateTime();
+
+        resultDiv.innerHTML = `
       <p><strong>Name</strong> : ${name}</p>
       <p><strong>Date of Birth</strong> : ${dob}</p>
       <p><strong>Gender</strong> : ${gender}</p>
       <p><strong>Message</strong> : ${pesan}</p>
     `;
-  });
+    });
 });
